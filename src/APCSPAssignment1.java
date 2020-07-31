12d8aaaa4a1deaa722170fd7883866d3b9460947
Tyler Cingel <tccingel@gmail.com>
2020-07-30 23:28:38 -0400

    First commit of WIP game.

--- src/APCSPAssignment1.java
+++
@@ -1,9 +0,0 @@
-
-public class APCSPAssignment1 {
-    public static void main(String[] args) {
-        KeyboardReader reader = new KeyboardReader();
-        System.out.println("Hello World!");
-        String text = reader.readLine("Enter a string: ");
-        System.out.println("You typed: " + text);
-    }
-}
